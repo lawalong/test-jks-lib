61265c5439d849fcf81daf6f725416ae7bfeab09
kevin Li <kevin.li@webjet.com.au>
2020-05-08 17:09:46 +1000

    test pipeline

--- vars/kubernetesPipeline.groovy
+++ vars/kubernetesPipeline.groovy
@@ -1,41 +1,210 @@
 import groovy.io.FileType
 
-def call() {
+def call(Map params) {
+
+  // Requiered parameters
+  /*
+  appName           = params.appName
+  appProject        = params.appProject
+  appVersion        = params.appVersion
+  technology        = params.technology
+  unitTests         = params.unitTests
+  runSonarAnalysis  = params.runSonarAnalysis
+  devLocations      = params.devLocations
+  liveAutoDeploy    = params.liveAutoDeploy
+  liveLocations     = params.liveLocations
+  soapUI            = params.soapUI
+  soapUIScripts     = params.soapUIScripts
+  soapUISecrets     = params.soapUISecrets
+  customChartName   = params.customChartName
+  
+  continuousIntegrationImage = sonar.getSonarScannerImage(technology)*/
+
+
+  repositoryBranch            = params.repositoryBranch
+  repositoryUrl               = params.repositoryUrl
   
   pipeline {
     agent any
+    environment {
+        CONTAINERREGISTRY     = credentials('9bfddad9-c9b4-4f57-b6cc-283d0997c5b5')
+        CONTAINERREGISTRYUSERNAME = credentials('ac3722c7-104f-4835-8438-70042e18a846')
+        CONTAINERREGISTRYPASSWORD = credentials('399bf241-b44f-4977-84c1-551792f726df')
+    }
+    stages{
+      stage('checkout') {
+          steps{
+              checkout([$class: 'GitSCM', branches: [[name: '*/'+repositoryBranch]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'f136133b-0c9f-4d5b-ba68-6d9d1fab21b6', url: repositoryUrl]]])
+          }          
+      }
 
-    stages {
+      stage('loging into container registry') {
+          steps{
+              sh 'docker login -u $CONTAINERREGISTRYUSERNAME -p $CONTAINERREGISTRYPASSWORD $CONTAINERREGISTRY'
+          }
+      }
+    }
+  }
+
+
+  /*  stages {
       stage('Continuous Integration'){
-
+		when {
+			not {
+				expression {continuousIntegrationImage == "" }
+			}
+			beforeAgent true
+		}
+        agent { 
+          docker { 
+            image continuousIntegrationImage
+            registryUrl 'https://webbeds.azurecr.io'
+            registryCredentialsId 'acrcredentials' 
+          }
+        }
         steps {
-          echo "1";
+          script {
+             sonar.analyze(technology, appName,appVersion,SONAR_IP,runSonarAnalysis,unitTests)
+          }
         }
       }
 
       stage('Build Artifact'){
+        when{
+          branch "master"
+        }
 		    steps{
-          echo "2";
+          script{
+            dockerUtils.createImage(appName,appProject,appVersion,false)
+          }
         }
       }
-
       stage('Update Docker Image TAG in DEV'){
+        when{
+          branch "master"
+        }
+        options { skipDefaultCheckout() }
 		    steps{
-          echo "3";
+          script{
+            devLocations.each{
+              kubernetes.upgradeChartVersion(appName,appVersion,"${it}","dev",false,customChartName)
+            }
+          }
         }
        } 
       stage('Deploy new version in DEV'){
-		    steps{
-          echo "4";
+        agent {
+          docker {
+            image 'webbeds.azurecr.io/argo/argocd:1.3.6'
+            registryUrl 'https://webbeds.azurecr.io'
+            registryCredentialsId 'acrcredentials'
+          }
+        }
+        options {
+          timeout(time: 1, unit: 'HOURS') 
+        }
+		    when{
+          branch "master"
+        }
+        steps{
+          script{
+           def stepsForParallel = [:]
+           devLocations.each{
+            def stepName = "Deploying in ${it}"
+            stepsForParallel[stepName] = { -> 
+              kubernetes.argoSync(appName,ARGO_IP,"${it}","dev") 
+            }
+           }
+          parallel stepsForParallel
+          }
         }
       }
 
 	  stage('Integration Tests'){
-		    steps{
-          echo "5";
-        }
+		when {
+			expression { soapUI == true }
 		}
+		agent {
+		  docker {
+			image "webbeds.azurecr.io/soapui/soapui:latest"
+			registryUrl 'https://webbeds.azurecr.io'
+			registryCredentialsId 'acrcredentials'
+		  }
+		}
+		steps {
+		  script {
+        if (soapUISecrets) {
+          withCredentials([file(credentialsId: soapUISecrets, variable: 'fileName')]) {
+			      soapui.runTests(soapUIScripts, fileName, 100)
+          }
+        } else {
+			    soapui.runTests(soapUIScripts, 100)
+        }
+		  }
+		}
+		post {
+		  always {
+			archiveArtifacts 'soapuireports/*'
+			junit 'soapuireports/TEST-*.xml'
+		  }
+		}
+	  }
+
+      stage('Update Docker Image TAG in LIVE'){
+        when{
+          branch "master"
+        }
+        options { skipDefaultCheckout() }
+        steps{
+          script{
+            // Before update we upload the live image to dockerRegistry
+            dockerUtils.createImage(appName,appProject,appVersion,true)
+            liveLocations.each{
+              kubernetes.upgradeChartVersion(appName,appVersion,"${it}","live",true,customChartName)
+            }
+          }
+        }
+      }
+      stage('Deploy new version in LIVE'){
+        agent {
+          docker {
+            image 'webbeds.azurecr.io/argo/argocd:1.3.6'
+            registryUrl 'https://webbeds.azurecr.io'
+            registryCredentialsId 'acrcredentials'
+          }
+        }
+        options {
+          timeout(time: 1, unit: 'HOURS') 
+        }
+        when{
+          branch "master"
+        }
+        steps{
+         script{
+           if (liveAutoDeploy){
+            def stepsForParallel = [:]
+            liveLocations.each{
+            def stepName = "Deploying in ${it}"
+            stepsForParallel[stepName] = { -> 
+              kubernetes.argoSync(appName,ARGO_IP,"${it}","live") 
+            }
+           }
+           parallel stepsForParallel 
+          }else{
+            echo "liveAutoDeploy disabled. Go to https://argocd.webbeds.com/applications?proj=${appProject} to Deploy manually"
+          }
+         }
+        }
+      }
+    }      
+    post {
+      always {
+        echo 'Clean up workspace...'
+        deleteDir()
+      }
     }
-    
-  }
+  }*/
+
+
+
 }
