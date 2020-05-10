214bea1e9cecdba7a090cc2e9a8c916cecc92861
kevin Li <kevin.li@webjet.com.au>
2020-05-11 02:01:58 +1000

    123

--- vars/dockerUtils.groovy
+++ vars/dockerUtils.groovy
@@ -6,7 +6,8 @@ echo "xxx world hello"
 def buildImage(imageName,args,dockerFilePath) {
                   sh(script: """
                   cd $WORKSPACE/${dockerFilePath}
-                  docker build ${args} -t "${CONTAINERREGISTRY}/webjet/${imageName}}":$BUILD_NUMBER .
+                  docker build ${args} -t "${CONTAINERREGISTRY}/webjet/${imageName}":$BUILD_NUMBER .
+                  docker push ${CONTAINERREGISTRY}/webjet/${imageName}:${BUILD_NUMBER}
                         """, returnStdout: true)
 
 
