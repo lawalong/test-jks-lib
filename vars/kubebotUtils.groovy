fe9136dd711ff8e4899d69955ee351c9c845dd2e
kevin Li <kevin.li@webjet.com.au>
2020-05-11 19:27:52 +1000

    123

--- vars/kubebotUtils.groovy
+++ vars/kubebotUtils.groovy
@@ -1,6 +1,6 @@
 def deploy(region,environment,nameSpace,appName) {
     echo "333 Deploying ${appName}-wjau to ${nameSpace} ..."
-    url2 = "123"
+    url2 = "http://kubebot.default/deploy/dev/${nameSpace}/${appName}/${BUILD_NUMBER}?registry=$CONTAINERREGISTRY&repository=webjet"
     sh '''
     echo \"4442 Deploying '''+appName+''' -wjau to '''+nameSpace+''' ...\"
      url = http://kubebot.default/deploy/dev/'''+nameSpace+'''/'''+appName+'''/${BUILD_NUMBER}?registry=$CONTAINERREGISTRY&repository=webjet
