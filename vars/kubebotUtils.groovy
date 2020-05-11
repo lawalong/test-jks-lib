c3ee62e5a6502e9e2a064d56f4efb423082acce4
kevin Li <kevin.li@webjet.com.au>
2020-05-11 19:02:00 +1000

    123

--- vars/kubebotUtils.groovy
+++ vars/kubebotUtils.groovy
@@ -3,13 +3,23 @@ def deploy(region,environment,nameSpace,appName) {
 
     sh '''
     echo \"4442 Deploying '''+appName+''' -wjau to '''+nameSpace+''' ...\"
-    url = http://kubebot.default/deploy/prod/'''+nameSpace+'''/'''+appName+'''/${BUILD_NUMBER}?registry=$CONTAINERREGISTRY&repository=webjet
+    url = http://kubebot.default/deploy/dev/'''+nameSpace+'''/'''+appName+'''/${BUILD_NUMBER}?registry=$CONTAINERREGISTRY&repository=webjet
     echo url
+
+
+                                response=$(curl -s -X POST url \
+                            --data-binary "@$WORKSPACE/pipeline/deploy.yaml" \
+                            -H 'Content-Type: application/yaml' \
+                            -H 'Expect:' \
+                            -D -)
+                            http_status=$(echo $response | grep HTTP | awk '{print $2}')
+                            if [ $http_status = 200 ]; then
+                                echo "Deployed"
+                            else
+                                echo "Something went wrong with the deployment, query the Kb-Trace-Id in sumo for more details."
+                                exit 1
+                            fi 
     '''
 
-    sh(script: """
 
-                        
-                        
-                        """, returnStdout: true)
 }
