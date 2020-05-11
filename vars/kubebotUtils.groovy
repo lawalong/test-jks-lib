dced322ea07361b79adae67a4b441d8bc61f0a16
kevin Li <kevin.li@webjet.com.au>
2020-05-11 18:23:25 +1000

    asd

--- vars/kubebotUtils.groovy
+++ vars/kubebotUtils.groovy
@@ -1,8 +1,5 @@
 def deploy(region,environment,nameSpace,appName) {
     echo "333 Deploying ${appName}-wjau to ${nameSpace} ..."
 
-    sh '
-          echo "4442 Deploying ${appName}-wjau to ${nameSpace} ..."
-
-    ' 
+    sh "echo \"4442 Deploying ${appName}-wjau to ${nameSpace} ...\""
 }
